4b764d2e359a683417eb8e02b93f5fc14d831906
xftfchgg <k6663668@gmail.com>
2024-01-23 09:57:42 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.EFModels;
+
+namespace WebApplication1.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly myDbContext _dbContext;
+
+        public ApiController(myDbContext context)
+
+        {
+
+            _dbContext = context;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _dbContext.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
